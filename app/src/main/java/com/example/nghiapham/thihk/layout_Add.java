5a9408d9bd80da2d5b5c133e61bb8fd28dfea522
Nghia Pham <nghiapham.it@gmail.com>
2018-03-15 09:08:19 +0700

    Bài thi HK

---
+++ app/src/main/java/com/example/nghiapham/thihk/layout_Add.java
@@ -0,0 +1,13 @@
+package com.example.nghiapham.thihk;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class layout_Add extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_layout__add);
+    }
+}
