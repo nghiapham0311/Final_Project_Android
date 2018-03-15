2ba0e8cfdaecd0e3e170fe39c0b16fcf6223b4d9
Nghia Pham <nghiapham.it@gmail.com>
2018-03-15 09:10:10 +0700

    Bài thi HK

---
+++ app/src/main/java/com/example/nghiapham/thihk/layout_edit.java
@@ -0,0 +1,13 @@
+package com.example.nghiapham.thihk;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class layout_edit extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_layout_edit);
+    }
+}
